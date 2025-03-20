e708c92623a47c17dcbb21d102cf776747922e00
oliwermiller <oliwer.miller@student.put.poznan.pl>
2025-03-20 18:01:53 +0100

    shell sort i funkcja pomocnicza

--- example.py
+++ example.py
@@ -13,19 +13,35 @@ def insertion_sort (data):
         data[j + 1] = key
     return data
 
+def sedgewick_gaps(n):
+    gaps = []
+    k = 0
+    while True:
+        if k % 2 == 0:
+            gap = 9 * (2 ** k) - 9 * (2 ** (k // 2)) + 1
+        else:
+            gap = 4 ** k + 3 * 2 ** (k - 1) + 1
+        
+        if gap >= n:
+            break
+        gaps.append(gap)
+        k += 1
+
+    return gaps[::-1]
+
 def shell_sort(data):
-    n=len(data)
-    gap = n//2
-    while gap>0:
+    n = len(data)
+    gaps = sedgewick_gaps(n)
+    
+    for gap in gaps:
         for i in range(gap, n):
-            temp=data[i]
-            j=i
-            while j>=gap and data[j-gap]> temp:
-                data[j]=data[j-gap]
-                j-=gap
-            data[j]=temp
-        gap//=2
-    return data
+            temp = data[i]
+            j = i
+            while j >= gap and data[j - gap] > temp:
+                data[j] = data[j - gap]
+                j -= gap
+            data[j] = temp
+
  
 def selection_sort(data):
     n=len(data)
